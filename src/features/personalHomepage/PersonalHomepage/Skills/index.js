940edc4555053d03d12d4ef0107099b78d6c6a02
Angelika Krygier <angelika.krygier96@gmail.com>
2023-04-25 11:04:14 +0200

    removed "ToLearn" component ane added its content to "Skills" component

--- src/features/personalHomepage/PersonalHomepage/Skills/index.js
+++ src/features/personalHomepage/PersonalHomepage/Skills/index.js
@@ -1,10 +1,18 @@
 import { Section } from "../Section";
 import { skillsList } from "./skillsList";
+import {toLearnList} from "./toLearnList"
 
 export const Skills = () => (
-    <Section
-        tittle="My skillset includes 🛠️"
-        skills={skillsList}
-    >
-    </Section>
+    <>
+        <Section
+            tittle="My skillset includes 🛠️"
+            skills={skillsList}
+        >
+        </Section>
+        <Section
+            tittle="What I want to learn next&nbsp;🚀"
+            skills={toLearnList}
+        >
+        </Section>
+    </>
 );
